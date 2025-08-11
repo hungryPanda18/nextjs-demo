d72ab25e52cf23aa59cba47b51f69e2a4c35ae02
hungryPanda18 <lieutenant.decoy18@gmail.com>
2025-08-11 16:49:57 +0530

    Delete app/dashboard/[slug] directory

--- app/dashboard/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-interface BlogPageProps {
-  params: {
-    slug: string;
-  };
-}
-
-export default function Page({ params }: BlogPageProps) {
-  return <h1>Post: {params.slug} page it is </h1>;
-}
